db0cc46107fb2659d17134dd6fcc65d6c113b259
Harry <hengwang@cn.ibm.com>
2020-02-28 17:29:36 +0800

    3-5 ListViewWidget列表组件讲解

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TextWidget',
+        title: 'IMooc Flutter Demo',
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('TextWidget'),
-            ),
-            body: Center(
-                child: Container(
-                  child: new Image.network(
-                    'http://img4.mukewang.com/szimg/5d25400a08fa408c06000338-360-202.jpg',
-                    scale: 2.0,
-                    repeat: ImageRepeat.repeatY,
-                  ),
-                  width: 400.0,
-                  height: 300.0,
-                  color: Colors.lightBlue,)
-            )));
+            appBar: new AppBar(title: new Text('ListView Widget')),
+            body: new ListView(
+              children: <Widget>[
+                new Image.network('https://img3.sycdn.imooc.com/szimg/5e26a9f909ef95b512000676-360-202.png'),
+                new Image.network('https://img3.sycdn.imooc.com/szimg/5e4f4f66098b14c512000676-360-202.png'),
+                new Image.network('https://img4.sycdn.imooc.com/szimg/5d1032ab08719e0906000338-360-202.jpg'),
+                 new Image.network('https://img3.sycdn.imooc.com/szimg/5e40f9d408e7c86b06000338-360-202.jpg')
+              ]
+            )
+            )
+            );
   }
 }
