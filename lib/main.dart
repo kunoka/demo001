2b84f97943f2c5205b840d8aa6248881c3dffa76
Harry <hengwang@cn.ibm.com>
2020-02-28 18:53:57 +0800

    3-6 横向列表和自定义组件讲解

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'IMooc Flutter Demo',
         home: Scaffold(
-            appBar: new AppBar(title: new Text('ListView Widget')),
-            body: new ListView(
-              children: <Widget>[
-                new Image.network('https://img3.sycdn.imooc.com/szimg/5e26a9f909ef95b512000676-360-202.png'),
-                new Image.network('https://img3.sycdn.imooc.com/szimg/5e4f4f66098b14c512000676-360-202.png'),
-                new Image.network('https://img4.sycdn.imooc.com/szimg/5d1032ab08719e0906000338-360-202.jpg'),
-                 new Image.network('https://img3.sycdn.imooc.com/szimg/5e40f9d408e7c86b06000338-360-202.jpg')
-              ]
-            )
-            )
-            );
+            appBar: new AppBar(
+              title: new Text('ListView Widget')
+              ),
+            body: Center(
+                child: Container(
+                  height: 200.0, 
+                  child: Mylist()
+                  )
+                  )
+                  )
+                  );
+  }
+}
+
+class Mylist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+      new Container(width: 180.0, color: Colors.lightBlue),
+      new Container(width: 180.0, color: Colors.amber),
+      new Container(width: 180.0, color: Colors.deepPurpleAccent),
+        new Container(width: 180.0, color: Colors.redAccent)
+    ]);
   }
 }
