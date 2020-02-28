7205087410ea2ed565ab8e894e6c869dbb48b555
Harry <hengwang@cn.ibm.com>
2020-02-28 11:17:59 +0800

    3-2 ContainerWidget容器组件讲解-1

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TextWidget',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('TextWidget'),
-          ), // AppBar
-          body: Center(
-            child: Text(
-              '去年已有百人感染15人死亡 专家：直报：直报系统不负责发现新发传染病去年已有百人感染15人死亡 专家：直报系统不负责发现新发传染病',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 25.0,
-                color: Color.fromARGB(255, 255, 150, 150),
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.solid
-                ),
-
+        title: 'TextWidget',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('TextWidget'),
+            ),
+            body: Center(
+                child: Container(
+              child: new Text(
+                'Hello Imooc',
+                style: TextStyle(fontSize: 40.0),
               ),
-          ) // Center
-      ) // Scaffold
-    );  // MaterialApp
+              alignment: Alignment.center,
+              width: 500.0,
+              height: 400.0,
+              color: Colors.lightBlue
+            ))));
   }
 }
