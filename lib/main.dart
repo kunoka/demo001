4aa1a10a1032763e2d9e965153303d3b3f697c49
Harry <hengwang@cn.ibm.com>
2020-02-28 19:12:13 +0800

    3-7 动态列表的使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(
+  items: new List<String>.generate(1000, (index) => "Item $index")
+));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+  MyApp({Key key, @required this.items}):super(key:key);
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,17 +17,16 @@ class MyApp extends StatelessWidget {
             appBar: new AppBar(
               title: new Text('ListView Widget')
               ),
-            body: Center(
-                child: Container(
-                  height: 200.0, 
-                  child: Mylist()
-                  )
-                  )
-                  )
-                  );
-  }
+            body: new ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index){
+                return new ListTile(
+                  title: new Text('${items[index]}')
+                );
+              }
+              )));
 }
-
+}
 class Mylist extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
