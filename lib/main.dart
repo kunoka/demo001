4580dd44fb3714f3bcf0971822f450e5b36d4277
Harry <hengwang@cn.ibm.com>
2020-02-29 10:38:10 +0800

    4-3 电影海报图片的加入与课程总结

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('电影海报实例')
         ),
-        body: GridView.count(
-          padding: const EdgeInsets.all(10.0),
-          crossAxisSpacing: 10.0,
-          crossAxisCount: 3,
-          children: <Widget>[
-            const Text('I love IMooc!'),
-            const Text('I love IMooc!'),
-            const Text('I love IMooc!'),
-            const Text('I love IMooc!'),
-            const Text('I love IMooc!'),
-            const Text('I love IMooc!'),
-          ],
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 2.0,
+            crossAxisSpacing: 2.0,
+            childAspectRatio: 0.7
+            ),
+            children: <Widget>[
+              new Image.network('http://img5.mtime.cn/mt/2019/12/06/151723.54470898_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/27/102155.97632815_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/17/105242.88827408_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/16/102337.73663733_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/27/101619.41873766_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2020/01/16/151210.15087026_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/06/151723.54470898_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/27/102155.97632815_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/17/105242.88827408_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/16/102337.73663733_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2019/12/27/101619.41873766_180X260X4.jpg', fit: BoxFit.cover,),
+              new Image.network('http://img5.mtime.cn/mt/2020/01/16/151210.15087026_180X260X4.jpg', fit: BoxFit.cover,)
+            ],
         )
       )
       );
