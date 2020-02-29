355a20877b3391ac9602540958d32752f1e43512
Harry <hengwang@cn.ibm.com>
2020-02-29 10:13:41 +0800

    4-1 电影海报实例代码基本结构的建立

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp(
-  items: new List<String>.generate(1000, (index) => "Item $index")
-));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-  MyApp({Key key, @required this.items}):super(key:key);
-
-
-  @override
-  Widget build(BuildContext context) {
+  @override 
+  Widget build(BuildContext context){
     return MaterialApp(
-        title: 'IMooc Flutter Demo',
-        home: Scaffold(
-            appBar: new AppBar(
-              title: new Text('ListView Widget')
-              ),
-            body: new ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index){
-                return new ListTile(
-                  title: new Text('${items[index]}')
-                );
-              }
-              )));
-}
-}
-class Mylist extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(scrollDirection: Axis.horizontal, children: <Widget>[
-      new Container(width: 180.0, color: Colors.lightBlue),
-      new Container(width: 180.0, color: Colors.amber),
-      new Container(width: 180.0, color: Colors.deepPurpleAccent),
-        new Container(width: 180.0, color: Colors.redAccent)
-    ]);
+      title: '电影海报实例',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('电影海报实例')
+        ),
+        body: Text('电影海报实例')
+      )
+      );
   }
 }
